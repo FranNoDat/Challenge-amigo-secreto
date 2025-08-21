e0e60ed6017760a3d90e85d865e50549c843f9c1
FranNoDat <franro826@gmail.com>
2025-08-21 18:30:37 -0300

    Primer avance

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//Función principal que agregará los nombres al array
+function agregarAmigo() {
+    let nombreAgregado = document.getElementById("amigo").value;
+    if (nombreAgregado == "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombreAgregado);
+        console.log(amigos);
+        asignarTextoElemento("#amigo", "");
+        return;
+    }
+}
+
+//Función principal que agrega los nombres a la lista HTML
+function actualizarLista() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = "";
+    
+}
+
+//Función genérica para establecer el texto de un elemento en determinado valor
+function asignarTextoElemento(elemento, texto) {
+    document.querySelector(elemento).value = texto;
+    //let elementoUsado = 
+    //console.log(elementoUsado);
+    //elementoUsado.value = texto;
+    return;
+}
